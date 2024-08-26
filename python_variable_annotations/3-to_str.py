a269bbbd8c3f5d32b4ee909e171cef96975b98dd
Hache David <hache.david@free.fr>
2024-08-26 13:00:44 +0000

    Add exercice 3 with pycodestyle checked

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' takes a float n as argument and returns
+the string representation of the float.'''
+
+
+def to_str(n: float) -> str:
+    string_return = str(n)
+    return string_return

c2e59693b3d5fb229c85f736701e639155f6e05c
Hache David <hache.david@free.fr>
2024-09-10 12:30:06 +0000

    Add exercice 5 with pycodestyle

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' takes a list input_list of floats as
+argument and returns their sum as a float'''
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    y = 0
+    for i in input_list:
+        y = i + y
+    return y

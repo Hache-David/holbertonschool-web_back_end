6dd8cbbc72b3e5a10087849b4d56b9bf1fabdfcd
Hache David <hache.david@free.fr>
2024-09-10 12:39:15 +0000

    Add exercice 6 with pycodestyle checked

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' type-annotated function sum_mixed_list which takes a list mxd_lst of integers and floats and returns their sum as a float'''
+
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    '''sum_mixed_list which takes a list mxd_lst'''
+    y = 0
+    for i in mxd_lst:
+        y = i + y
+    return y

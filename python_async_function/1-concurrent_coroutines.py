b085a8e06c447811af66e96ed361b5da7a6b3ff8
Hache David <hache.david@free.fr>
2024-09-16 12:41:18 +0000

    fix commentary

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -11,6 +11,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    '''erfververververververv'''
     list_return = []
     tasks = [wait_random(max_delay) for _ in range(n)]
     for task in asyncio.as_completed(tasks):

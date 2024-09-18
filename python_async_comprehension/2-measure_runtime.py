6ac56ff516371a800421a500044d353ab75cc8c1
Hache-David <146715401+Hache-David@users.noreply.github.com>
2024-09-18 22:42:11 +0200

    Update 2-measure_runtime.py with perf_counter() in place of time()

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,8 +9,9 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    '''efvqfvqsfvqdf'''
-    start = time.time()
+    '''Executes async_comprehension four times in parallel and
+    measures total runtime.'''
+    start = time.perf_counter()
     await asyncio.gather(*(async_comprehension() for i in range(4)))
-    end = time.time()
+    end = time.perf_counter()
     return (end - start)

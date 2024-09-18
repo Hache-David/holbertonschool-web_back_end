1d8387d726985b9f1e84d3769e42a1ca43b541b7
Hache David <hache.david@free.fr>
2024-09-18 12:11:14 +0000

    Add integer variable

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-'''measure_time function with integers n and max_delay as arguments that measures
-the total execution time for wait_n(n, max_delay), and returns total_time / n.
-Your function should return a float.'''
+'''youpi '''
 
 import time
 import asyncio
@@ -9,9 +7,10 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay) -> float:
-    start_time = time.perf_counter()
+def measure_time(n: int, max_delay: int) -> float:
+    '''sfcvfvsdfvdfv'''
+    start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
-    end_time = time.perf_counter()
+    end_time = time.time()
     total_time = end_time - start_time
     return total_time / n

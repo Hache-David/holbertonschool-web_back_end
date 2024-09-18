72e1f16b7c0de25e64bd02dd2913409c8c981e8b
Hache David <hache.david@free.fr>
2024-09-18 12:52:01 +0000

    try new notation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import random
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[None, None]:
+async def async_generator() -> Generator[float, None, None]:
     '''jzzhbzjhb'''
     for i in range(10):
         await asyncio.sleep(1)
